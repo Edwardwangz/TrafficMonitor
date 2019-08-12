3b3ecffc2b43b15879509c7152bdd59bbd874661
Edwardwangz <wangzhuang0806@126.com>
2019-08-12 19:42:41 +0800

    simple test

--- src/main/java/org/wangz/traffic/skynet/MonitorFlowAnalysis.java
+++ src/main/java/org/wangz/traffic/skynet/MonitorFlowAnalysis.java
@@ -7,16 +7,23 @@ import org.apache.spark.sql.SparkSession;
 import org.wangz.traffic.constant.Constant;
 
 public class MonitorFlowAnalysis {
+    /**
+     * 进行本地测试，模拟生成的数据也放入了 MySQL 中，分别为
+     * monitor_camera_info  标准信息表，记录所有卡扣下的所有摄像头  如00001 52265 ...
+     * monitor_flow_action  真实监测表，记录 * 2018-11-05	0003	21305	京W93011	2018-11-05 08:55:26	124	45	03
+     * @param args
+     */
 
     public static void main(String[] args) {
 
         SparkSession spark = SparkSession.builder()
                 .appName(Constant.SPARK_APPNAME)
                 .master(Constant.SPARK_RUN_MODE)
-                .enableHiveSupport()
                 .getOrCreate();
         JavaSparkContext sc = new JavaSparkContext(spark.sparkContext());
-        spark.sql("use traffic");
+
+
+
 
 
 
