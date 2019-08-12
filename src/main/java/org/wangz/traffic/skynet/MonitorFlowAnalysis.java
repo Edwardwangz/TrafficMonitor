fe9d17bddb969fc3d6671296c94e2b51fcf1334d
Edwardwangz <wangzhuang0806@126.com>
2019-08-12 14:08:31 +0800

    init MonitorFlowAnalysis

--- src/main/java/org/wangz/traffic/skynet/MonitorFlowAnalysis.java
+++ src/main/java/org/wangz/traffic/skynet/MonitorFlowAnalysis.java
@@ -1,9 +1,20 @@
 package org.wangz.traffic.skynet;
 
 
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.SparkSession;
+import org.wangz.constant.Constant;
+
 public class MonitorFlowAnalysis {
 
     public static void main(String[] args) {
+
+        SparkSession spark = SparkSession.builder()
+                .appName(Constant.SPARK_APPNAME)
+                .master(Constant.SPARK_RUN_MODE)
+                .enableHiveSupport()
+                .getOrCreate();
+        JavaSparkContext sc = new JavaSparkContext(spark.sparkContext());
 
     }
 }
