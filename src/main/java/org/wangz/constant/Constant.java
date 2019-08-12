a13fd38315e22e20d7a51f916106c2b57357e3a6
Edwardwangz <wangzhuang0806@126.com>
2019-08-12 12:23:27 +0800

    add some constants

--- src/main/java/org/wangz/constant/Constant.java
+++ src/main/java/org/wangz/constant/Constant.java
@@ -3,4 +3,22 @@ package org.wangz.constant;
 public interface Constant {
     String SPARK_APPNAME = "Traffic Monitor";
     String SPARK_RUN_MODE = "local";
+    String FIELD_CAMERA_COUNT = "cameraCount";
+    String FIELD_CAMERA_IDS = "cameraIds";
+    String FIELD_CAR_COUNT = "carCount";
+    String FIELD_NORMAL_MONITOR_COUNT = "normalMonitorCount";
+    String FIELD_NORMAL_CAMERA_COUNT = "normalCameraCount";
+    String FIELD_ABNORMAL_MONITOR_COUNT = "abnormalMonitorCount";
+    String FIELD_ABNORMAL_CAMERA_COUNT = "abnormalCameraCount";
+    String FIELD_ABNORMAL_MONITOR_CAMERA_INFOS = "abnormalMonitorCameraInfos";
+    String FIELD_TOP_NUM = "topNum";
+    String FIELD_DATE_HOUR="dateHour";
+    String FIELD_CAR_TRACK="carTrack";
+    String FIELD_DATE="dateHour";
+    String FIELD_CAR="car";
+    String FIELD_CARS="cars";
+    String FIELD_MONITOR="monitor";
+    String FIELD_MONITOR_ID="monitorId";
+    String FIELD_ACTION_TIME="actionTime";
+    String FIELD_EXTRACT_NUM = "extractNum";
 }
