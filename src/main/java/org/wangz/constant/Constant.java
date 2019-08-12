af81959ec687668599a2b653b95e64e267a6bbe4
Edwardwangz <wangzhuang0806@126.com>
2019-08-12 11:13:29 +0800

    Init

---
+++ src/main/java/org/wangz/constant/Constant.java
@@ -0,0 +1,6 @@
+package org.wangz.constant;
+
+public interface Constant {
+    String SPARK_APPNAME = "Traffic Monitor";
+    String SPARK_RUN_MODE = "local";
+}
---
+++ src/main/java/org/wangz/traffic/skynet/MonitorFlowAnalysis.java
@@ -0,0 +1,9 @@
+package org.wangz.traffic.skynet;
+
+
+public class MonitorFlowAnalysis {
+
+    public static void main(String[] args) {
+
+    }
+}
